34792d125a20f252c8297592b0039619a3a76947
v-aidaba <v-aidaba@microsoft.com>
2025-09-17 15:55:12 +0400

    upgrade versions, also fixed lnter errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,28 @@
 import powerbiVisualsConfigs from "eslint-plugin-powerbi-visuals";
-import tseslint from 'typescript-eslint';
+import tseslint from "typescript-eslint";
 
 export default [
-    ...tseslint.configs.recommended,
-    powerbiVisualsConfigs.configs.recommended,
-    {
-        ignores: [
-            "node_modules/**",
-            "dist/**",
-            "coverage/**",
-            "test/**",
-            ".tmp/**",
-            "karma.conf.ts",
-            "test.webpack.config.js"
-        ],
+  ...tseslint.configs.recommended,
+  powerbiVisualsConfigs.configs.recommended,
+  {
+    ignores: [
+      "node_modules/**",
+      "dist/**",
+      "coverage/**",
+      "test/**",
+      ".tmp/**",
+      "karma.conf.ts",
+      "test.webpack.config.js",
+    ],
+  },
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: "./tsconfig.json",        
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
+  },
 ];
